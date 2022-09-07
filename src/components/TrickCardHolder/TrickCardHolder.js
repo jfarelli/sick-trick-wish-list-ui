ece6f64d6790f1aaef60839d26c7a6e38c618413
Jordan Farelli <jordanfarelli@gmail.com>
2022-09-07 10:51:08 -0600

    Established holder for trickcards and styled

---
+++ src/components/TrickCardHolder/TrickCardHolder.js
@@ -0,0 +1,27 @@
+import './TrickCardHolder.css'
+import TrickCards from '../TrickCards/TrickCards'
+
+const TrickCardHolder = ( { tricks } ) => {
+
+    const trickCard = tricks.map( trick => {
+        return (
+            <TrickCards
+                stance={ trick.stance  } 
+                trick={ trick.name }
+                obstacle={ trick.obstacle }
+                tutorial={ trick.tutorial }
+                id={ trick.id }
+                key={ trick.id }/>
+        )
+    } )
+
+    return (
+        
+        <div className='trick-card-container'>
+            { trickCard }
+        </div>
+    )
+}
+
+
+export default TrickCardHolder;
