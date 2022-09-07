56f534c7280937bdac55057c1b6f7d97f064059f
Jordan Farelli <jordanfarelli@gmail.com>
2022-09-07 10:51:50 -0600

    Established trickcards component and styled

--- src/components/TrickCards/TrickCards.js
+++ src/components/TrickCards/TrickCards.js
@@ -1,8 +1,14 @@
 import './TrickCards.css'
 
-const TrickCards = ( ) => {
+const TrickCards = ( { stance, trick, obstacle, tutorial, id } ) => {
+
     return (
-        <h1>TRICKCARD</h1>
+        <div className='trick-card'>
+            <p>{ `${stance} ${trick} ` }</p>
+            <p>Obstacle: { obstacle }</p>
+            <p>Link to Tutorial:</p>
+            <a href={ tutorial }>{ tutorial }</a>
+        </div>
     )
 }
 
